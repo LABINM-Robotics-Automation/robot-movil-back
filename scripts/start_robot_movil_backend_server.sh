28706e28f8e4f66a406272e595618de3d8fb9db2
pQbas <percybrayan18@gmail.com>
2024-11-16 20:23:21 -0500

    updated script to start the robot-movil-backend

--- scripts/start_robot_movil_backend_server.sh
+++ scripts/start_robot_movil_backend_server.sh
@@ -12,7 +12,11 @@ cd "$PROJECT_DIR" || exit
 # Activate the virtual environment
 source /home/labinm-jetson/xavier_ssd/robot-movil-back/robot_movil_back/bin/activate 
 
-# Start or attach to the tmux session and run the Django server
-tmux new-session -d -s "$SESSION_NAME" "python manage.py runserver 0.0.0.0:8000"
+# Check if the tmux session already exists
+if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    tmux new-session -d -s "$SESSION_NAME" "python manage.py runserver 0.0.0.0:8000"
+    echo "Django server started in tmux session: $SESSION_NAME"
+else
+    echo "Tmux session $SESSION_NAME already exists."
+fi
 
-echo "Django server started in tmux session: $SESSION_NAME"
